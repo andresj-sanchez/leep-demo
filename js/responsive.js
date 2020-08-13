66e0fe3edbdc09232d4fcfb692c4a78543f24ad3
Andres Sanchez <contacttowork.andres+mygithub@gmail.com>
2020-08-13 16:00:03 -0400

    bug fix: responsive.js

--- js/responsive.js
+++ js/responsive.js
@@ -4,13 +4,14 @@ function getViewportSize() {
         height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
     };
 }
-
+let firstTime = true;
 function renderPageForMobileDevices(){
     const {width} = getViewportSize();
     if(width < 576){
         div1.before(div2);
         div1.after(div3);
-    }else{
+        if(firstTime) firstTime = false;
+    }else if(!firstTime){
         divDownloadContent.appendChild(div2);
         div2.after(div3);
     }
